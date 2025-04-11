f56e0ba8cd3fdbce5723f6059ccfdecc464ef8bc
Ojaswakesharwani <ojaswakesharwani@gmail.com>
2025-04-12 03:37:17 +0530

    add shimmer dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //Shrimmer Effect
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
+
 }
